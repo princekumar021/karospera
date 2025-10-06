9c67d19b7fedb1c95b4f2365e523c3947d2d2a30
Prince kumar <princekumarsaha564@gmail.com>
2025-10-06 17:44:47 +0000

    use this path /home/user/studio/public/images/favicon.ico

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -27,7 +27,7 @@ export default function RootLayout({
   return (
     <html lang="en" className={`${inter.variable} ${playfairDisplay.variable}`}>
       <head>
-      <link rel="icon" href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>⚪</text></svg>" />
+      <link rel="icon" href='/images/favicon.ico'/>
       </head>
       <body className="font-body antialiased bg-background text-foreground">
         <UserDataProvider>
